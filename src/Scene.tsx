5fd8560660e365952a30ab00ff01e6dfecc6300c
Mark Murphy <murphy.mark@northeastern.edu>
2022-04-19 16:50:29 -0400

    Log relative, image-scaled coordinates on click

--- src/Scene.tsx
+++ src/Scene.tsx
@@ -1,15 +1,34 @@
-import { MouseEvent as SyntheticMouseEvent } from 'react';
+import useSize from '@react-hook/size';
+import { MouseEvent as SyntheticMouseEvent, useRef } from 'react';
 import beachImage from './resources/beach.jpg';
 import styles from './Scene.module.css';
 
+const BEACH_NATURAL_WIDTH = 1920;
+const BEACH_NATURAL_HEIGHT = 1080;
+
 const Scene = () => {
+  const sceneRef = useRef(null);
+  const [currentWidth, currentHeight] = useSize(sceneRef);
+
   const handleClick = (e: SyntheticMouseEvent<Element, MouseEvent>) => {
-    // TODO: Using pageX and pageY breaks if the scene doesn't start at (0, 0) in the page.
-    console.log(`x: ${e.pageX}\ty: ${e.pageY}`);
+    // Get relative click coords inside the Scene div
+    const currentTargetRect = e.currentTarget.getBoundingClientRect();
+    const relativeX = e.pageX - currentTargetRect.left;
+    const relativeY = e.pageY - currentTargetRect.top;
+
+    // Get click coords scaled up to the natural size of the image.
+    const naturalX = Math.floor(
+      (BEACH_NATURAL_WIDTH / currentWidth) * relativeX
+    );
+    const naturalY = Math.floor(
+      (BEACH_NATURAL_HEIGHT / currentHeight) * relativeY
+    );
+
+    console.log(`natural_x: ${naturalX}\tnatural_y: ${naturalY}`);
   };
 
   return (
-    <div onClick={handleClick}>
+    <div ref={sceneRef} onClick={handleClick} className={styles['scene']}>
       <img
         src={beachImage}
         alt="Where's Waldo scene"
