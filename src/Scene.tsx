417597d78ed858ed813df65fe59d3245bc0bb9b2
Mark Murphy <murphy.mark@northeastern.edu>
2022-04-19 15:45:16 -0400

    Log x and y on image click

--- src/Scene.tsx
+++ src/Scene.tsx
@@ -1,9 +1,15 @@
+import { MouseEvent as SyntheticMouseEvent } from 'react';
 import beachImage from './resources/beach.jpg';
 import styles from './Scene.module.css';
 
 const Scene = () => {
+  const handleClick = (e: SyntheticMouseEvent<Element, MouseEvent>) => {
+    // TODO: Using pageX and pageY breaks if the scene doesn't start at (0, 0) in the page.
+    console.log(`x: ${e.pageX}\ty: ${e.pageY}`);
+  };
+
   return (
-    <div>
+    <div onClick={handleClick}>
       <img
         src={beachImage}
         alt="Where's Waldo scene"
